ebf16d7c28a7d009b52619c8d52051717748970e
Evgeniy Poremchuk <e.poremchuk@gmail.com>
2015-05-08 23:53:42 +0300

    Add exceptions

--- triangle.rb
+++ triangle.rb
@@ -13,10 +13,28 @@
 # and
 #   about_triangle_project_2.rb
 #
+
+
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  #TODO: Are the incoming values > 0 ?
+  puts TriangleError, "Sides must be greater than 0." unless [a,b,c].map { |x|
+    x > 0
+  }
+
+  if a == b && a == c
+    return :equilateral
+  elsif a == b || a == c || c == b
+    :isosceles
+  else
+    :scalene
+  end
 end
+
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
+
+
+puts triangle(1, 0, 3)
